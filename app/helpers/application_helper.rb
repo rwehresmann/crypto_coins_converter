d92794d2f3a01c773faacfae16239bc6fb107561
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-07-15 22:34:31 -0300

    Updated helper to get coins list from the Coin model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def currency_select
+    Coin.order(abreviation: :asc).each.inject([]) { |array, coin|
+      array << ["#{coin.abreviation} (#{coin.name})", coin.coin_market_cap_id]
+    }
+  end
 end
