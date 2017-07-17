aeffbe9f5fb85acaf39f6a800bd65c1177f63982
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-07-16 22:41:29 -0300

    Updated app name and refs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_currency_converter_session'
+Rails.application.config.session_store :cookie_store, key: '_crypto_coins_converter_session'
