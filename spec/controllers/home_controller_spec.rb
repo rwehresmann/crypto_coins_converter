def3bae7b8a65ce59522a1c4ffd6812e04f1f3d5
rodrigo-ehresmann <igoehresmann@gmail.com>
2017-07-16 22:46:54 -0300

    Updatde HomeController tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,29 +4,21 @@ RSpec.describe HomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
+
       expect(response.status).to eql(200)
     end
   end
 
   describe "POST #convert" do
-    subject {
-      post :convert, params: {
-        currency: @currency,
-        currency_destination: @currency_destination,
-        quantity: @quantity
-      }
-    }
-
-    before do
-      json = JSON.parse(File.read("./spec/fixtures/currency_list.json"))
-      @currency, @currency_destination = json['currency_list'].sample(2)
-      @quantity = rand(1..9999)
-    end
-
-    it { expect(subject).to have_http_status(:success) }
-
     it "returns a numeric value" do
-      value = JSON.parse(subject.body)['value']
+      post :convert, params: {
+        currency: "ethereum",
+        currency_destination: "bitcoin",
+        quantity: 1
+      }
+
+      expect(response.status).to eq 200
+      value = JSON.parse(response.body)['value']
       expect(value.is_a? Numeric).to be_truthy
     end
   end
